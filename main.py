23a8456b45a893d7beeaed1c087b0cea90f55473
M3M0Ri <mahdidotcomputer@gmail.com>
2024-04-23 14:18:45 +0330

    Make obstacles

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import pygame, sys
 from player import Player
 from laser import Laser
+import obstacle
 
 
 class Game:
     def __init__(self):
+        #arcos
         player_spirit = Player((screen_width / 2, screen_height), screen_width, 5)
         self.player = pygame.sprite.GroupSingle(player_spirit)
 
+        #obstacle
+        self.shape  = obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        self.obstacle_amount = 3
+        self.obstacle_x_positions = [(num * (screen_width / self.obstacle_amount)) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacle(*self.obstacle_x_positions, x_start=screen_width/15, y_start=480)
+
+    def create_obstacle(self, x_start, y_start, offset_x):
+        for row_index, row in enumerate(self.shape):
+            for col_index, col in enumerate(row):
+                if col == "x":
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
+                    block = obstacle.Block(self.block_size, (93, 63, 211), x, y)
+                    self.blocks.add(block)
+
+    def create_multiple_obstacle(self, *offset, x_start, y_start):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -19,6 +41,7 @@ class Game:
             self.player.update()
             self.player.sprite.lasers.draw(screen)
             self.player.draw(screen)
+            self.blocks.draw(screen)
 
             pygame.display.flip()
             clock.tick(60)
