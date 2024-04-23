aa5811e51884fac5644b174bdbbaa0ea27cac8ee
M3M0Ri <mahdidotcomputer@gmail.com>
2024-04-23 12:54:19 +0330

    initial game environment

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame, sys
+from player import Player
+
+
+class Game:
+    def __init__(self):
+        player_spirit = Player((screen_width / 2, screen_height), screen_width, 5)
+        self.player = pygame.sprite.GroupSingle(player_spirit)
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            screen.fill((30, 30, 30))
+            self.player.update()
+            self.player.draw(screen)
+
+            pygame.display.flip()
+            clock.tick(60)
+
+
+if __name__ == "__main__":
+    pygame.init()
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+
+    game = Game()
+    game.run()
